550dd06b1991279ff3daff823f3b9c301cf66c9a
Cadence <cadencehattaway@gmail.com>
2022-09-19 09:57:12 -0600

    Add files via upload

--- MathQuiz/src/Main.java
+++ MathQuiz/src/Main.java
@@ -7,53 +7,55 @@ public class Main {
         Scanner indexScan = new Scanner(System.in);
         Random randomGen = new Random();
 
-        int xNumberOne ;
-        int xNumberTwo ;
-        int xOperator;
-        int xPlayerAnswer;
-        int xCorrectAnswer ;
+        int rand;
+        rand = randomGen.nextInt(3);
+        System.out.println(rand);
+        boolean quit = false;
+        int numberOne;
+        numberOne = randomGen.nextInt(100);
+        int numberTwo;
+        numberTwo = randomGen.nextInt(100);
+        int guessOne;
+        int guessTwo = 0;
+        String userInput;
 
+        while (!quit) {
 
-            xNumberOne = randomGen.nextInt(100);
-            xNumberTwo = randomGen.nextInt(100);
+            switch (rand) {
+                case 0:
+                    System.out.println("Here is your equation " + numberOne + "+" + numberTwo);
+                    System.out.println("What is your Answer?");
+                    guessOne = indexScan.nextInt();
+                    if(guessOne != numberOne + numberTwo){
+                        System.out.println("Wrong answer try again");
+                        guessTwo = indexScan.nextInt();
+                    }else if(guessTwo != numberOne + numberTwo){
+                        System.out.println(numberOne + numberTwo);
+                        System.out.println("Do you want to play again(y) or quit(q)?");
+                        userInput =indexScan.next();
+                        if( userInput.contains("q")){
+                            quit = true;
+                        }else if(userInput.contains("y")){
+                            quit= false;
+                        }
+                    }
+                    quit = true;
+                case 1:
+                    System.out.println("Here is your equation " + numberOne + "-" + numberTwo);
+                    System.out.println("What is your Answer?");
+                    guessOne = indexScan.nextInt();
+                    quit = true;
+                case 2:
+                    System.out.println("Here is your equation " + numberOne + "*" + numberTwo);
+                    System.out.println("What is your Answer?");
+                    guessOne = indexScan.nextInt();
+                    quit = true;
 
-            Random randomOperate = new Random();
-
-            xOperator = randomOperate.nextInt(3);
-
-            System.out.println("Welcome to Math Quiz");
-            System.out.print("Here's your equation: " + xNumberOne);
-
-            if (xOperator == 0) {
-                System.out.print(" + ");
-            } else if (xOperator == 1) {
-                System.out.print(" - ");
-            } else {
-                System.out.print(" * ");
+                    break;
+                    
+                default:
             }
-
-            System.out.println(xNumberTwo);
-
-            System.out.println("What is your answer: ");
-            xPlayerAnswer = indexScan.nextInt();
-
-            if (xPlayerAnswer == xNumberOne * xNumberTwo){
-                System.out.println("You got it right!");
-            }else if(xPlayerAnswer == xNumberOne + xNumberTwo){
-                System.out.println("You got it right!");
-            }else if(xPlayerAnswer == xNumberOne - xNumberTwo) {
-                System.out.println("You got it right!");
-            }
-
-                if (xPlayerAnswer != xNumberOne * xNumberTwo) {
-                    System.out.println("Sorry that's wrong, try again");
-                } else if (xPlayerAnswer != xNumberOne + xNumberTwo) {
-                    System.out.println("Sorry that's wrong, try again");
-                } else if (xPlayerAnswer != xNumberOne - xNumberTwo) {
-                    System.out.println("Sorry that's wrong, try again");
-                }
-
-
+        }
 
     }
 }
