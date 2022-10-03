0681e7c250938bc4576c3de8a66bd4fba4b376cd
Cadence <cadencehattaway@gmail.com>
2022-10-03 17:40:22 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,56 +1,28 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner inputScan = new Scanner(System.in);
-
-        String playerInput;
+        Scanner indexScan = new Scanner(System.in);
         boolean quit = false;
-
-        System.out.println("Please enter a letter: ");
-        playerInput = inputScan.nextLine();
-        playerInput = playerInput.toLowerCase();
-
-
-        while(!quit){
-
-            if (playerInput.contains("a") || playerInput.contains("e") || playerInput.contains("i") || playerInput.contains("o") || playerInput.contains("u")) {
-                System.out.println("This letter is a Vowel");
-                System.out.println("Do you want to enter another letter (y) or quit (q)?");
-                playerInput = inputScan.nextLine();
-                playerInput = playerInput.toLowerCase();
-
-                if (playerInput.contains("q") ){
+        String ssnInput;
+        while (!quit) {
+            System.out.println("Please enter your Social Security Number: ");
+            ssnInput = indexScan.next();
+            if(ssnInput.length() == 11) {
+                if (ssnInput.charAt(3) == '-' && ssnInput.charAt(6) == '-') {
+                    System.out.println("Correct Format!");
                     quit = true;
-                } else if (playerInput.contains("y")){
-                    quit = false;
-                    System.out.println("Please enter another letter");
-                    playerInput = inputScan.nextLine();
-                    playerInput = playerInput.toLowerCase();
-
                 }
-
-            } else {
-                System.out.println("This letter is a Consonant");
-                System.out.println("Do you want to enter another letter(y) or quit(q)?");
-                playerInput = inputScan.nextLine();
-                playerInput = playerInput.toLowerCase();
-
-                if (playerInput.contains("q") ){
-                    quit = true;
-            }else if(playerInput.contains("y")){
-                    quit = false;
-                    System.out.println("Please enter another letter");
-                    playerInput = inputScan.nextLine();
-                    playerInput = playerInput.toLowerCase();
-
-                }
-
-        }
-
-
-
-
+            }
+            if (ssnInput.length() > 11) {
+                System.out.println("Incorrect format, try again");
+            } else if (!(ssnInput.contains("-"))) {
+                System.out.println("Wrong format please try again");
+            } else if (ssnInput.charAt(1) != '-' & ssnInput.charAt(2) != '-' &
+                    ssnInput.charAt(3) != '-' & ssnInput.charAt(4) != '-' & ssnInput.charAt(5) != '-' &
+                    ssnInput.charAt(6) != '-' & ssnInput.charAt(7) != '-' & ssnInput.charAt(8) != '-' &
+                    ssnInput.charAt(9) != '-' & ssnInput.charAt(10) != '-' ) {
+                System.out.println("Wrong format please try again");
+            }
+        }//While loop
     }
-  }
 }
